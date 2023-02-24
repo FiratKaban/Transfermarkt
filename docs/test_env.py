e7132003115819133d548eec976f50fc32c11717
Oguzhan Ince <oguzhan.ince@protonmail.com>
2023-02-24 14:50:01 +0300

    mac

--- docs/test_env.py
+++ docs/test_env.py
@@ -30,9 +30,22 @@ def get_seasons():
 
     soup = BeautifulSoup(response.content, 'html.parser')
     
+    # social media urls
+		# div class="socialmedia-icons" href
+    try:
+      data = soup.find_all('div', class_='socialmedia-icons')
+      for a in data[0].find_all('a'):
+        social_media_url = a.get('href')
+        print(social_media_url)
+        # TODO: degiskenin adini kullanarak db'de ilgili sutuna yazdirabilirsin
+    except:
+      pass
+ 
     try:
       player_agent_url = soup.find_all('span', class_='info-table__content--flex')
-      print(player_agent_url[0].find('a')['href'])
+      player_agent_url = player_agent_url[0].find('a')['href']
+      print(player_agent_url)
+      # TODO: degiskenin adini kullanarak db'de ilgili sutuna yazdirabilirsin
     except:
       pass
     
@@ -41,12 +54,14 @@ def get_seasons():
     for i in range(len(text)):
       
       try:
+        # sadece index varmi kontrolu icin re.search place 
         place_of_birth_text = re.search('Place', text[i].text).start()
         place_of_birth_text = text[x+1].text
         first_alpha = re.search('[a-zA-Z0-9]', place_of_birth_text).start()
         last_alpha = re.search('[a-zA-Z0-9]', place_of_birth_text[::-1]).start()
         place_of_birth_text = place_of_birth_text[first_alpha:len(place_of_birth_text)-last_alpha]
         print(place_of_birth_text)
+        # TODO: degiskenin adini kullanarak db'de ilgili sutuna yazdirabilirsin
       except:
         pass
       
@@ -57,6 +72,7 @@ def get_seasons():
         last_alpha = re.search('[a-zA-Z0-9]', place_of_birth_text[::-1]).start()
         place_of_birth_text = place_of_birth_text[first_alpha:len(place_of_birth_text)-last_alpha]
         print(place_of_birth_text)
+        # TODO: degiskenin adini kullanarak db'de ilgili sutuna yazdirabilirsin
       except:
         pass
       
@@ -67,42 +83,25 @@ def get_seasons():
         last_alpha = re.search('[a-zA-Z0-9]', contract_expires_text[::-1]).start()
         contract_expires_text = contract_expires_text[first_alpha:len(contract_expires_text)-last_alpha]
         print(contract_expires_text)
+        # TODO: degiskenin adini kullanarak db'de ilgili sutuna yazdirabilirsin
+      except:
+        pass
+      
+      try:
+        outfitter_text = re.search('Outfitter', text[i].text).start()
+        outfitter_text = text[x+1].text
+        first_alpha = re.search('[a-zA-Z0-9]', outfitter_text).start()
+        last_alpha = re.search('[a-zA-Z0-9]', outfitter_text[::-1]).start()
+        outfitter_text = outfitter_text[first_alpha:len(outfitter_text)-last_alpha]
+        print(outfitter_text)
+        # TODO: degiskenin adini kullanarak db'de ilgili sutuna yazdirabilirsin
       except:
         pass
       
       x += 1
-        
-
-    '''
-    for allseason in soup.select("div.info-table info-table--right-space"):
-        #lig kodunu split yapıp sözlüğe yazdıracaz.
-        rrss=outfitter=contract_expires=manager=place=False
-        if (etiket.text.find('Place of birth:')!=-1):
-           place_of_birth.append(etiket.text.replace('\n','').replace('\xa0',''))
-           place=True
-        elif (etiket.text.find('Player agent:')!=-1):
-            manager_company.append(etiket.text.replace('\n','').replace('\xa0',''))
-            manager=True
-        elif (etiket.text.find('Contract expires:')!=-1):
-            expires.append(etiket.text.replace('\n','').replace('\xa0',''))
-            contract_expires=True
-        elif (etiket.text.find('Outfitter:')!=-1):
-            player_outfitter.append(etiket.text.replace('\n','').replace(' ',''))
-            outfitter=True
-        elif (etiket.text.find('Social-Media:')!=-1):
-            player_rrss.append(etiket.find_all('a')[0]['href'])
-            rrss=True
-		'''
-
-      
-      # //*[@id="main"]/main/div[3]/div[1]/div[2]/div/div[2]/div/span[6]/span/text()
-  
-
 
 
 if __name__ == "__main__":
             # get_league_pagination(main_url)
             # get_league_links(pagination)
             get_seasons()
-            
-            
