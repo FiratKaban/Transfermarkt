518d89955db82fb19c1f1a3d7b8312204f2161a1
Oguzhan Ince <oguzhan.ince@protonmail.com>
2023-05-31 13:41:16 +0300

    mac

--- transfermarkt2.py
+++ transfermarkt2.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 """
 Created on Tue Sep 13 13:24 2022
+Updated on Sun Feb 26 12:13 2023
 Updated on Wed May 31 13:29 2023
 
 @author: FIRATKABAN
@@ -8,9 +9,3 @@ Updated on Wed May 31 13:29 2023
 """
 
 
-from collections import namedtuple
-from bs4 import BeautifulSoup       # bs4 kütüphanesini import ediyoruz
-import requests                     # requests kütüphanesini import ediyoruz
-from requests import Session        # 
-import time, csv, psycopg2, sys
-from parsel   import Selector
