9a1c084714e18b225467db61699bee71e22ca5f9
Oguzhan Ince <oguzhan.ince@protonmail.com>
2023-06-05 01:27:49 +0300

    mac

--- transfermarkt2.py
+++ transfermarkt2.py
@@ -3,6 +3,7 @@
 Created on Tue Sep 13 13:24 2022
 Updated on Sun Feb 26 12:13 2023
 Updated on Wed May 31 13:29 2023
+Updated on Mon Jun 05 01:29 2023
 ...
 @author: FIRATKABAN
 @contributer: cerebnismus
