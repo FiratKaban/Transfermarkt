b0dace58c76cb4ef83073c76ee66ff71923dfacb
Oguzhan Ince <oguzhan.ince@protonmail.com>
2023-06-05 18:56:09 +0300

    mac

--- transfermarkt2.py
+++ transfermarkt2.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 """.
-Created on Tue Sep 13 13:24 2022
-Updated on Sun Feb 26 12:13 2023
-Updated on Wed May 31 13:29 2023
-Updated on Mon Jun 05 01:29 2023
+Created on Tue Sep 13 13:24 2022 - Initial commit, less code, more comments
+Updated on Sun Feb 26 12:13 2023 - AWS RDS PostgreSQL database added and tested
+Updated on Wed May 31 13:29 2023 - New scraper added and unnecessary codes removed
+Updated on Mon Jun 05 01:29 2023 - Database related improvements and fixes
 ...
 @author: FIRATKABAN
 @contributer: cerebnismus
